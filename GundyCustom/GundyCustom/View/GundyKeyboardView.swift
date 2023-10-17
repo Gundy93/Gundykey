3cd2768d6f2e55ead0554726cd1932c626b7943e
Gundy <korea1880@naver.com>
2023-10-17 12:23:26 +0900

    feat: 방향에 따른 모음을 정의하는 기능 구현

--- GundyCustom/GundyCustom/View/GundyKeyboardView.swift
+++ GundyCustom/GundyCustom/View/GundyKeyboardView.swift
@@ -10,6 +10,7 @@ import UIKit
 final class GundyKeyboardView: UIView {
     
     weak var delegate: GundyKeyboardViewDelegate?
+    private var vowels: [String] = ["ㅗ", "ㅣ", "ㅏ", "ㅡ", "ㅜ", "ㅡ", "ㅓ", "ㅣ"]
     private var directions: [Direction] = []
     
     required init?(coder: NSCoder) {
@@ -20,24 +21,126 @@ final class GundyKeyboardView: UIView {
     
     private func configureGestureRecognizer() {
         let gestureRecognizer = UIPanGestureRecognizer(target: self,
-                                             action: #selector(inputVowels))
+                                             action: #selector(drag))
         
         addGestureRecognizer(gestureRecognizer)
     }
     
     @objc
-    private func inputVowels(_ sender: UIPanGestureRecognizer) {
-        let velocity = sender.velocity(in: self)
-        guard let direction = convertToDirection(from: velocity),
-              directions.last != direction else { return }
+    private func drag(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .changed:
+            guard directions.count < 4 else { return }
+            
+            let velocity = sender.velocity(in: self)
+            
+            recordDirection(for: velocity)
+        case .ended:
+            inputVowel()
+        default:
+            return
+        }
+    }
+    
+    private func recordDirection(for velocity: CGPoint) {
+        let direction = convertToDirection(from: velocity)
+        guard let last = directions.last else {
+            directions.append(direction)
+            return
+        }
         
+        switch direction {
+        case .up:
+            guard last != .up, last != .leftUp, last != .rightUp else { return }
+        case .rightUp:
+            guard last != .rightUp, last != .up, last != .right else { return }
+        case .right:
+            guard last != .right, last != .rightUp, last != .rightDown else { return }
+        case .rightDown:
+            guard last != .rightDown, last != .right, last != .down else { return }
+        case .down:
+            guard last != .down, last != .rightDown, last != .leftDown else { return }
+        case .leftDown:
+            guard last != .leftDown, last != .down, last != .left else { return }
+        case .left:
+            guard last != .left, last != .leftDown, last != .leftUp else { return }
+        case .leftUp:
+            guard last != .leftUp, last != .left, last != .up else { return }
+        }
         
+        directions.append(direction)
+    }
+    
+    private func inputVowel() {
+        var vowel = vowels[directions[0].rawValue]
+        
+        if directions.count > 1 {
+            vowel = secondInput(with: vowel)
+        }
+        
+        if directions.count > 2 {
+            vowel = thirdInput(with: vowel)
+        }
+        
+        if directions.count == 4 {
+            vowel = forthInput(with: vowel)
+        }
+        print(vowel)
+        directions.removeAll()
+        delegate?.insertVowel(vowel)
+    }
+    
+    private func secondInput(with vowel: String) -> String {
+        switch vowel {
+        case "ㅏ":
+            return "ㅐ"
+        case "ㅓ":
+            return "ㅔ"
+        case "ㅗ":
+            return directions[1] == .right ? "ㅘ" : "ㅚ"
+        case "ㅜ":
+            return directions[1] == .left ? "ㅝ" : "ㅟ"
+        case "ㅡ":
+            return "ㅢ"
+        default:
+            return vowel
+        }
+    }
+    
+    private func thirdInput(with vowel: String) -> String {
+        switch vowel {
+        case "ㅐ":
+            return directions[2] == .right ? "ㅑ" : vowel
+        case "ㅔ":
+            return directions[2] == .left ? "ㅕ" : vowel
+        case "ㅚ":
+            return directions[2] == .up ? "ㅛ" : vowel
+        case "ㅟ":
+            return directions[2] == .down ? "ㅠ" : vowel
+        case "ㅘ":
+            return directions[2] != .up ? "ㅙ" : vowel
+        case "ㅝ":
+            return directions[2] != .down ? "ㅞ" : vowel
+        default:
+            return vowel
+        }
+    }
+    
+    private func forthInput(with vowel: String) -> String {
+        switch vowel {
+        case "ㅑ":
+            return "ㅒ"
+        case "ㅕ":
+            return "ㅖ"
+        default:
+            return vowel
+        }
     }
 }
 
 extension GundyKeyboardView {
     
-    enum Direction {
+    enum Direction: Int {
         case up
         case rightUp
         case right
@@ -48,13 +151,13 @@ extension GundyKeyboardView {
         case leftUp
     }
     
-    private func convertToDirection(from velocity: CGPoint) -> Direction? {
+    private func convertToDirection(from velocity: CGPoint) -> Direction {
         let x = velocity.x
         let y = velocity.y
         
-        if abs(x) / 4.1421 >= abs(y) {
+        if abs(x) / 2.414 >= abs(y) {
             return x > 0 ? .right : .left
-        } else if abs(y) / 4.1421 >= abs(x) {
+        } else if abs(y) / 2.414 >= abs(x) {
             return y > 0 ? .down : .up
         } else if x > 0 {
             return y > 0 ? .rightDown : .rightUp
--- GundyCustom/GundyCustom/View/GundyKeyboardViewDelegate.swift
+++ GundyCustom/GundyCustom/View/GundyKeyboardViewDelegate.swift
@@ -7,7 +7,7 @@
 
 protocol GundyKeyboardViewDelegate: AnyObject {
     func insertConsonant(_ newCharacter: String)
-    func insertVowels(_ newCharacter: String)
+    func insertVowel(_ newCharacter: String)
     func insertOther(_ newCharacter: String)
     func removeCharacter()
 }
